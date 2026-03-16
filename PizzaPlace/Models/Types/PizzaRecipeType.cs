03239cd5a18354564ab19cf9b992ca913cd1f710
RebekkaHeart <rebekkapahuspedersen@gmail.com>
2026-03-16 13:32:22 +0100

    Hardcoded a menu for lunch and a standard one.

--- PizzaPlace/Models/Types/PizzaRecipeType.cs
+++ PizzaPlace/Models/Types/PizzaRecipeType.cs
@@ -9,4 +9,5 @@ public enum PizzaRecipeType
     RarePizza,
     HorseRadishPizza,
     EmptyPizza,
+    ChildPizza
 }
--- PizzaPlace/Services/MenuService.cs
+++ PizzaPlace/Services/MenuService.cs
@@ -4,7 +4,50 @@ public class MenuService : IMenuService
 {
     public Menu GetMenu(DateTimeOffset menuDate)
     {
-        throw new NotImplementedException("No menu has been implemented yet.");
+        int currentHour = menuDate.Hour;
+
+        // Give the menu according to what time it is
+        if (currentHour >= 11 && currentHour < 14)
+        {
+            // Return the lunch menu
+            return new Menu("Lunch Menu", GetMenuItems(true));
+        }
+        else
+        {
+            // Return the standard menu
+            return new Menu("Standard Menu", GetMenuItems(false));
+        }
+    }
+
+    private ComparableList<MenuItem> GetMenuItems(bool isLunchTime)
+    {
+        ComparableList<MenuItem> list = new ComparableList<MenuItem>();
+
+        // Make the items that are always for sale and add them to the list
+        list.Add(new MenuItem("Pepperoni Pizza.", Models.Types.PizzaRecipeType.StandardPizza, 10.99));
+        list.Add(new MenuItem("Mario Pizza.", Models.Types.PizzaRecipeType.StandardPizza, 9.99));
+        list.Add(new MenuItem("Luigi Pizza.", Models.Types.PizzaRecipeType.StandardPizza, 9.99));
+        list.Add(new MenuItem("Wario Pizza.", Models.Types.PizzaRecipeType.OddPizza, 8.99));
+        list.Add(new MenuItem("Waluigi Pizza.", Models.Types.PizzaRecipeType.OddPizza, 10.99));
+        list.Add(new MenuItem("Peach Pizza.", Models.Types.PizzaRecipeType.ExtremelyTastyPizza, 12.99));
+        list.Add(new MenuItem("Daisy Pizza.", Models.Types.PizzaRecipeType.ExtremelyTastyPizza, 12.99));
+        list.Add(new MenuItem("Yoshi Pizza.", Models.Types.PizzaRecipeType.ExtremelyTastyPizza, 12.99));
+        list.Add(new MenuItem("Mushroom Pizza.", Models.Types.PizzaRecipeType.StandardPizza, 10.99));
+        list.Add(new MenuItem("Bowser Pizza.", Models.Types.PizzaRecipeType.RarePizza, 15.99));
+
+        // Depending on whether it's lunch time, add more pizzas
+        if (isLunchTime)
+        {
+            list.Add(new MenuItem("Boo Pizza", Models.Types.PizzaRecipeType.RarePizza, 15.99));
+            list.Add(new MenuItem("Baby Mario Pizza", Models.Types.PizzaRecipeType.ChildPizza, 7.99));
+        }
+        else
+        {
+            list.Add(new MenuItem("Shy Guy Pizza", Models.Types.PizzaRecipeType.OddPizza, 9.99));
+            list.Add(new MenuItem("Bowser Junior Pizza", Models.Types.PizzaRecipeType.ChildPizza, 7.99));
+        }
+
+        return list;
     }
 }
  
