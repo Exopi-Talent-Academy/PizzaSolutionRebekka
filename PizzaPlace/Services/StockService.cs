3f3c74e130d9a427ab5cec9680c446b641afbf8e
RebekkaHeart <rebekkapahuspedersen@gmail.com>
2026-03-17 16:25:54 +0100

    Created HasInsufficientStock test and begun working on its implementation in StockService

--- PizzaPlace/Services/StockService.cs
+++ PizzaPlace/Services/StockService.cs
@@ -7,9 +7,50 @@ public class StockService(IStockRepository stockRepository) : IStockService
 {
     public Task<bool> HasInsufficientStock(PizzaOrder order, ComparableList<PizzaRecipeDto> recipeDtos)
     {
-        throw new NotImplementedException("Sufficient stock must be checked.");
+        ComparableList<StockDto> stockNeededForOrder = new ComparableList<StockDto>();
+
+        // NEED TO TAKE INTO ACCOUNT THAT THE ORDER DECIDES HOW MANY OF SOMETHING THERE IS
+        // BOTH ORDER AND RECIPEDTOS HAVE A PIZZARECIPETYPE, PAIR 'EM UP
+
+        // Add together the PizzaAmounts in order where the PizzaType is the same?? Would change what I do for quantity
+
+        // Run through each StockType used in the order and add the ones who are the same together in a new stockdto
+        foreach (PizzaRecipeDto recipe in recipeDtos) 
+        {
+            // Get the quantity of a specific pizza in an order
+            int quantity = 0;
+
+            foreach (var pizza in order.RequestedOrder)
+            {
+                if (pizza.PizzaType == recipe.RecipeType)
+                {
+                    quantity = pizza.Amount;
+                    break;
+                }
+            }
+
+            foreach (StockDto stock in recipe.Ingredients)
+            {
+                StockDto newStock = stock with { Amount = (stock.Amount * quantity) };
+
+                if (stockNeededForOrder.Any(item => item.StockType == stock.StockType))
+                {
+                    // If this stocktype is already in the list, add to it
+                    //CODE
+                }
+                else
+                {
+                    // If it's not already in the list, add it
+                    stockNeededForOrder.Add(newStock);
+                }
+            }
+        }
+
+        // Then call GetStock from FakeStockRepository and compare with the above, if there's ever too many, return false
 
         // Call GetStock from FakeStockRepository for each thing in the order and check if there's enough stock
+
+        return false;
     }
 
     public Task<ComparableList<StockDto>> GetStock(PizzaOrder order, ComparableList<PizzaRecipeDto> recipeDtos)
--- Tests/PizzaPlace.Test/Services/StockServiceTests.cs
+++ Tests/PizzaPlace.Test/Services/StockServiceTests.cs
@@ -1,6 +1,44 @@
-﻿namespace PizzaPlace.Test.Services;
+﻿using PizzaPlace.Controllers;
+using PizzaPlace.Models;
+using PizzaPlace.Models.Types;
+using PizzaPlace.Repositories;
+using PizzaPlace.Services;
+
+namespace PizzaPlace.Test.Services;
 
 [TestClass]
 public class StockServiceTests
 {
+    private static StockService GetService(Mock<IStockRepository> stockRepository) =>
+        new(stockRepository.Object);
+
+    [TestMethod]
+    public async Task HasInsufficentStock_ReturnsTrueWhenInsufficient()
+    {
+        // Arrange : Make the order and recipes
+        var order = new PizzaOrder([
+            new PizzaAmount(PizzaRecipeType.RarePizza, 1),
+            new PizzaAmount(PizzaRecipeType.OddPizza, 2),
+            new PizzaAmount(PizzaRecipeType.RarePizza, 20),
+        ]);
+
+        StockDto stock1 = new StockDto(StockType.UnicornDust, 1);
+        StockDto stock2 = new StockDto(StockType.Sulphur, 10);
+        var rareRecipe = new PizzaRecipeDto(PizzaRecipeType.RarePizza, [stock1], 1);
+        var oddRecipe = new PizzaRecipeDto(PizzaRecipeType.OddPizza, [stock2], 100);
+        ComparableList<PizzaRecipeDto> recipeList = [rareRecipe, oddRecipe];
+
+        // Arrange : Make the repository with its stock
+        var stockRepository = new Mock<IStockRepository>(MockBehavior.Strict);
+        stockRepository.Setup(x => x.AddToStock(stock1)).ReturnsAsync(stock1);
+        stockRepository.Setup(x => x.AddToStock(stock2)).ReturnsAsync(stock2);
+
+        var service = GetService(stockRepository);
+
+        // Act
+        var actual = await service.HasInsufficientStock(order, recipeList);
+
+        // Assert
+        Assert.IsFalse(actual);
+    }
 }
