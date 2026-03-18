1d4cb8d7aa965d45f54ce42e569f88af9c5a395e
RebekkaHeart <rebekkapahuspedersen@gmail.com>
2026-03-18 15:21:35 +0100

    Begun work on RecipeController

---
+++ PizzaPlace/Controllers/RecipeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPlace.Services;
+
+namespace PizzaPlace.Controllers;
+
+[Route("api/recipes")]
+public class RecipeController(IRecipeService recipeService) : ControllerBase
+{
+    /*[HttpGet]
+    public IActionResult GetRecipes()
+    {
+        return Ok(recipeService.GetPizzaRecipes());
+    }*/
+
+    //[HttpPost]
+}
