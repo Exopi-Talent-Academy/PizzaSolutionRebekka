6cb9ad62f7fc029714aa8bef2ca06d94bd713cd8
RebekkaHeart <rebekkapahuspedersen@gmail.com>
2026-03-18 14:45:28 +0100

    Implemented GetStock and wrote it tests. Or rather, moved much of the code HasInsufficientStock into it

--- PizzaPlace/Services/StockService.cs
+++ PizzaPlace/Services/StockService.cs
@@ -14,51 +14,8 @@ public class StockService(IStockRepository stockRepository) : IStockService
     /// <returns>true, if the stock is insufficient; otherwise, false</returns>
     public async Task<bool> HasInsufficientStock(PizzaOrder order, ComparableList<PizzaRecipeDto> recipeDtos)
     {
-        List<StockDto> stockNeededForOrder = new List<StockDto>();
-        Dictionary<PizzaRecipeType, int> recipeTypeAmountsInOrder = new Dictionary<PizzaRecipeType, int>();
-
-        // Go through the ordered pizzas and add them to the dictionary with their amounts
-        foreach (var pizza in order.RequestedOrder)
-        {
-            if (recipeTypeAmountsInOrder.ContainsKey(pizza.PizzaType))
-            {
-                // If any of them are of the same type, their amounts get added together
-                recipeTypeAmountsInOrder[pizza.PizzaType] += pizza.Amount;
-            }
-            else
-            {
-                // If it's not in the list already, add it
-                recipeTypeAmountsInOrder.Add(pizza.PizzaType, pizza.Amount);
-            }
-        }
-
-        // Go through each recipe and add the needed stock to the list
-        foreach (PizzaRecipeDto recipe in recipeDtos) 
-        {
-            // Get the quantity of a specific pizzatype in an order
-            int quantity = recipeTypeAmountsInOrder[recipe.RecipeType];
-
-            // Go through the ingredients in the recipe and add them to the needed stock
-            foreach (StockDto stock in recipe.Ingredients)
-            {
-                // The number of the same pizza ordered decides how much of an ingredient is needed
-                StockDto newStock = stock with { Amount = (stock.Amount * quantity) };
-
-                // Check if the ingredient's stocktype is already in the list
-                if (stockNeededForOrder.Any(item => item.StockType == stock.StockType))
-                {
-                    // If this stocktype is already in the list, add to it
-                    int index = stockNeededForOrder.IndexOf(stockNeededForOrder.Find(item => item.StockType == stock.StockType));
-                    int newAmount = stockNeededForOrder[index].Amount + newStock.Amount;
-                    stockNeededForOrder[index] = newStock with { Amount = newAmount };
-                }
-                else
-                {
-                    // If it's not already in the list, add it
-                    stockNeededForOrder.Add(newStock);
-                }
-            }
-        }
+        ComparableList<StockDto> stockNeededForOrder = await GetStock(order, recipeDtos);
+        Dictionary<PizzaRecipeType, int> recipeTypeAmountsInOrder = GetRecipeTypeAmountsInOrder(order.RequestedOrder);
 
         // Go through the list of the stock needed for an order
         foreach (var neededStock in stockNeededForOrder)
@@ -76,10 +33,72 @@ public class StockService(IStockRepository stockRepository) : IStockService
         return false;
     }
 
-    public Task<ComparableList<StockDto>> GetStock(PizzaOrder order, ComparableList<PizzaRecipeDto> recipeDtos)
+    /// <summary>
+    /// Gets the stock needed for an order
+    /// </summary>
+    /// <param name="order"></param>
+    /// <param name="recipeDtos"></param>
+    /// <returns>Returns a ComparableList<StockDto> with the stock</returns>
+    public async Task<ComparableList<StockDto>> GetStock(PizzaOrder order, ComparableList<PizzaRecipeDto> recipeDtos)
     {
-        throw new NotImplementedException("Getting stock must be implemented.");
+        ComparableList<StockDto> stockNeededForOrder = new ComparableList<StockDto>();
+        Dictionary<PizzaRecipeType, int> recipeTypeAmountsInOrder = GetRecipeTypeAmountsInOrder(order.RequestedOrder);
 
-        // Call GetStock from FakeStockRepository
+        // Go through each recipe and add the needed stock to the list
+        foreach (PizzaRecipeDto recipe in recipeDtos)
+        {
+            // Get the quantity of a specific pizzatype in an order
+            int quantity = recipeTypeAmountsInOrder[recipe.RecipeType];
+
+            // Go through the ingredients in the recipe and add them to the needed stock
+            foreach (StockDto stock in recipe.Ingredients)
+            {
+                // The number of the same pizza ordered decides how much of an ingredient is needed
+                StockDto newStock = stock with { Amount = (stock.Amount * quantity) };
+
+                // Check if the ingredient's stocktype is already in the list
+                if (stockNeededForOrder.Any(item => item.StockType == stock.StockType))
+                {
+                    // If this stocktype is already in the list, add to it
+                    int index = stockNeededForOrder.IndexOf(stockNeededForOrder.FirstOrDefault(item => item.StockType == stock.StockType)!);
+                    int newAmount = stockNeededForOrder[index].Amount + newStock.Amount;
+                    stockNeededForOrder[index] = newStock with { Amount = newAmount };
+                }
+                else
+                {
+                    // If it's not already in the list, add it
+                    stockNeededForOrder.Add(newStock);
+                }
+            }
+        }
+
+        return stockNeededForOrder;
+    }
+
+    /// <summary>
+    /// Get the amounts of types of PizzaRecipeType in dictionary from an order
+    /// </summary>
+    /// <param name="requestedOrder"></param>
+    /// <returns></returns>
+    private Dictionary<PizzaRecipeType, int> GetRecipeTypeAmountsInOrder(ComparableList<PizzaAmount> requestedOrder)
+    {
+        Dictionary<PizzaRecipeType, int> recipeTypeAmountsInOrder = new Dictionary<PizzaRecipeType, int>();
+
+        // Go through the ordered pizzas and add them to the dictionary with their amounts
+        foreach (var pizza in requestedOrder)
+        {
+            if (recipeTypeAmountsInOrder.ContainsKey(pizza.PizzaType))
+            {
+                // If any of them are of the same type, their amounts get added together
+                recipeTypeAmountsInOrder[pizza.PizzaType] += pizza.Amount;
+            }
+            else
+            {
+                // If it's not in the list already, add it
+                recipeTypeAmountsInOrder.Add(pizza.PizzaType, pizza.Amount);
+            }
+        }
+
+        return recipeTypeAmountsInOrder;
     }
 }
--- Tests/PizzaPlace.Test/Services/StockServiceTests.cs
+++ Tests/PizzaPlace.Test/Services/StockServiceTests.cs
@@ -103,4 +103,54 @@ public class StockServiceTests
         // Assert
         Assert.IsFalse(result);
     }
+
+    [TestMethod]
+    [DataRow(1, 1, 5)]
+    [DataRow(5, 1, 5)]
+    [DataRow(4, 1, 3)]
+    public async Task GetStock_GivesCorrectStock(int orderAmount1, int orderAmount2, int orderAmount3)
+    {
+        // Arrange : Make the order and recipes
+        var order = new PizzaOrder([
+            new PizzaAmount(PizzaRecipeType.RarePizza, (ushort)orderAmount1), // need to do casting because DataRow doesn't take ushorts
+            new PizzaAmount(PizzaRecipeType.OddPizza, (ushort)orderAmount2),
+            new PizzaAmount(PizzaRecipeType.RarePizza, (ushort)orderAmount3)
+        ]);
+
+        StockDto recipeStock1 = new StockDto(StockType.UnicornDust, 1);
+        StockDto recipeStock2 = new StockDto(StockType.Sulphur, 10);
+        var rareRecipe = new PizzaRecipeDto(PizzaRecipeType.RarePizza, [recipeStock1], 1);
+        var oddRecipe = new PizzaRecipeDto(PizzaRecipeType.OddPizza, [recipeStock2], 100);
+        ComparableList<PizzaRecipeDto> recipeList = [rareRecipe, oddRecipe];
+
+        // Arrange : Make the mock recipe repository with recipes
+        var recipeRepository = new Mock<IRecipeRepository>(MockBehavior.Strict);
+        recipeRepository.Setup(x => x.GetRecipe(PizzaRecipeType.RarePizza))
+            .ReturnsAsync(rareRecipe);
+        recipeRepository.Setup(x => x.GetRecipe(PizzaRecipeType.OddPizza))
+            .ReturnsAsync(oddRecipe);
+
+        // Arrange : Make the mock stock repository with the stock
+        var stockRepository = new Mock<IStockRepository>(MockBehavior.Strict);
+
+        StockDto mockStock1 = new StockDto(StockType.UnicornDust, 10);
+        StockDto mockStock2 = new StockDto(StockType.Sulphur, 10);
+
+        stockRepository.Setup(x => x.GetStock(recipeStock1.StockType))
+            .ReturnsAsync(mockStock1);
+        stockRepository.Setup(x => x.GetStock(recipeStock2.StockType))
+            .ReturnsAsync(mockStock2);
+
+        var service = GetService(stockRepository);
+
+        // Act
+        var actual = await service.GetStock(order, recipeList);
+
+        // Assert
+        Assert.AreEqual(2, actual.Count); // the number of stock objects are the same
+        Assert.AreEqual((order.RequestedOrder[0].Amount + order.RequestedOrder[2].Amount) * recipeStock1.Amount, 
+                         actual.FirstOrDefault(item => item.StockType == recipeStock1.StockType)!.Amount);
+        Assert.AreEqual(order.RequestedOrder[1].Amount * recipeStock2.Amount, 
+                        actual.FirstOrDefault(item => item.StockType == recipeStock2.StockType)!.Amount);
+    }
 }
